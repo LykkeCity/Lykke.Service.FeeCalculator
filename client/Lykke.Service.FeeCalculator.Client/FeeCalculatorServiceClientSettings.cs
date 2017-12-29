b11c1911bc27ad0bb27a451f9daadf40518474d0
Oleksandr Lysenko <monarch.lysenko@gmail.com>
2017-12-29 14:59:52 +0100

    #LWDEV-4874: add FeeCalculatorServiceClientSettings class

---
+++ client/Lykke.Service.FeeCalculator.Client/FeeCalculatorServiceClientSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.FeeCalculator.Client
+{
+    public class FeeCalculatorServiceClientSettings
+    {
+        public string ServiceUrl { get; set; }
+    }
+}

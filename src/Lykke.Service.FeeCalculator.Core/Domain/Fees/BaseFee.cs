f74c403ca849b5e582d1042092d6365a5d0fbd32
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-02-02 21:31:53 +0300

    fix issue with fee calculation

---
+++ src/Lykke.Service.FeeCalculator.Core/Domain/Fees/BaseFee.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.Service.FeeCalculator.Core.Domain.Fees
+{
+    public class BaseFee : IBaseFee
+    {
+        public decimal TakerFee { get; set; }
+        public decimal MakerFee { get; set; }
+    }
+}

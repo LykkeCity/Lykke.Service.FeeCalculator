bec6f9c8bfa7b1ed44724d84528d80e90c5c47af
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-02-13 15:24:34 +0300

    make MarketOrderFees settings optional

--- src/Lykke.Service.FeeCalculator.Core/Settings/ServiceSettings/FeeCalculatorSettings.cs
+++ src/Lykke.Service.FeeCalculator.Core/Settings/ServiceSettings/FeeCalculatorSettings.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Lykke.Service.FeeCalculator.Core.Domain.Fees;
+using Lykke.SettingsReader.Attributes;
 
 namespace Lykke.Service.FeeCalculator.Core.Settings.ServiceSettings
 {
@@ -8,7 +10,8 @@ namespace Lykke.Service.FeeCalculator.Core.Settings.ServiceSettings
         public int TradeVolumeToGetInDays { get; set; }
         public CacheSettings Cache { get; set; }
         public DbSettings Db { get; set; }
-        public MarketOrderFee[] MarketOrderFees { get; set; }
+        [Optional] 
+        public MarketOrderFee[] MarketOrderFees { get; set; } = Array.Empty<MarketOrderFee>();
         public List<CashoutFee> CashoutFees { get; set; }
         public BankCardSettings BankCard { get; set; }
     }
